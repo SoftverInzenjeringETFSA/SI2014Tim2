2768a3bd959e9014e195040398a850ca0210a7e9
dbihorac <dunjabihorac@gmail.com>
2015-05-11 21:30:36 +0200

    Creating initial project and Maven build

---
+++ Code/quick-sheet/src/main/java/etf/si/t2/quick_sheet/App.java
@@ -0,0 +1,14 @@
+package etf.si.t2.quick_sheet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	new Login().setVisible(true);
+    }
+}
