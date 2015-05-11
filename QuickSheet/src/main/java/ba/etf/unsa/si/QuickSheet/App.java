38e17994f301eba668d575b911472778651ac9e7
dbihorac <dunjabihorac@gmail.com>
2015-05-11 21:58:21 +0200

    Initial commit of working project

---
+++ QuickSheet/src/main/java/ba/etf/unsa/si/QuickSheet/App.java
@@ -0,0 +1,13 @@
+package ba.etf.unsa.si.QuickSheet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
