6bf48df3a2e13f1dded4c50c6a20fb61598a65f6
Haris <custovicharis@gmail.com>
2015-05-20 22:59:47 +0200

    Uklonjene greške nastale usred merge-a

--- QuickSheet/src/main/java/ba/etf/unsa/si/Klase/Koordinator.java
+++ QuickSheet/src/main/java/ba/etf/unsa/si/Klase/Koordinator.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-package ba.etf.unsa.si.Klase;
-
-import java.time.LocalDate;
-
-import javax.naming.directory.InvalidAttributeValueException;
-
-public class Koordinator extends Zaposlenik{
-
-	public Koordinator(String _username, String _lozinka, String _ime, String _prezime, String _adresa, LocalDate _datum, double _cijena)
-			throws InvalidAttributeValueException {
-		super(_username, _lozinka, _ime, _prezime, _adresa, _datum, _cijena);
-	}
-	
-	public void OdobriTimesheet(Timesheet timesheet)
-	{
-		timesheet.setValidiran(true);
-	}
-	
-	public void KreirajTaskProjekta(Projekat projekat, Task task) throws InvalidAttributeValueException
-	{
-		if (projekat.getKoordinator().equals(this) && !projekat.getTaskovi().contains(task))
-			projekat.DodajTask(task);
-		else throw new InvalidAttributeValueException(); 
-	}
-	
-	public void DodajZaposlenikaUProjekat (Projekat projekat, ProjekatRadnik zaposlenik) throws InvalidAttributeValueException
-	{
-		if (projekat.getKoordinator().equals(this) && !projekat.getZaposlenici().contains(zaposlenik))
-			projekat.DodajZaposlenika(zaposlenik);
-		else throw new InvalidAttributeValueException(); 
-	}
-	
-	public void ArhivirajProjekat (Projekat projekat) throws InvalidAttributeValueException
-	{
-		if (projekat.getKoordinator().equals(this))
-			projekat.setArhiviran(true);
-		else throw new InvalidAttributeValueException();
-	}
-	
-	public IzvjestajZaposlenika GenerisiIzvjestaj(Projekat _projekat, Zaposlenik _zaposlenik) throws InvalidAttributeValueException
-	{
-		return new IzvjestajZaposlenika(_projekat, _zaposlenik);
-	}
-	
-	public IzvjestajOdjela GenerisiIzvjestaj(Projekat _projekat, Odjel _odjel) throws InvalidAttributeValueException
-	{
-		return new IzvjestajOdjela(_projekat, _odjel);
-	}
-	
-	public void PrintajIzvjestaj(String naziv)
-	{
-		//TODO dodati logiku printanja izvjestaja
-	}
-}
-=======
 package ba.etf.unsa.si.Klase;
 
 import java.time.LocalDate;
@@ -108,4 +52,3 @@ public class Koordinator extends Zaposlenik{
 		//TODO dodati logiku printanja izvjestaja
 	}
 }
->>>>>>> origin/master
--- QuickSheet/src/main/java/ba/etf/unsa/si/Klase/ProjekatRadnik.java
+++ QuickSheet/src/main/java/ba/etf/unsa/si/Klase/ProjekatRadnik.java
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-package ba.etf.unsa.si.Klase;
-
-import java.time.LocalDate;
-import java.util.LinkedList;
-
-import javax.naming.directory.InvalidAttributeValueException;
-
-public class ProjekatRadnik extends Zaposlenik{
-
-	public ProjekatRadnik(String _username, String _lozinka, String _ime, String _prezime, String _adresa, LocalDate _datum, double _cijena)
-			throws InvalidAttributeValueException {
-		super(_username, _lozinka, _ime, _prezime, _adresa, _datum, _cijena);
-	}
-	
-	public Timesheet PopuniTimesheet(LinkedList<Task> _taskovi, Integer radniSati, Projekat _projekat) throws javax.management.InvalidAttributeValueException
-	{
-		return new Timesheet(_taskovi, radniSati, _projekat);
-	}
-	
-	public void UrediTask(Task task, String komentar, Integer procenatZavrsenosti) throws InvalidAttributeValueException
-	{
-		if (this.equals(task.getZaposlenik()))
-		{
-			task.setKomentar(komentar);
-			task.setProcenatZavrsenosti(procenatZavrsenosti);
-		}
-		else throw new InvalidAttributeValueException();
-	}
-	
-	public void SlanjeTimesheetaNaReviziju(Timesheet timesheet)
-	{
-		timesheet.getProjekat().getTimesheetList().add(timesheet);
-	}
-}
-=======
 package ba.etf.unsa.si.Klase;
 
 import java.time.LocalDate;
@@ -68,4 +32,4 @@ public class ProjekatRadnik extends Zaposlenik{
 		timesheet.getProjekat().getTimesheetList().add(timesheet);
 	}
 }
->>>>>>> origin/master
+
